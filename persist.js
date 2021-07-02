cf7b32aaaa0bf69057708f1f1ab83858b8a1f9dd
Jace Fugate <jace.fugate@tcnbroadcasting.com>
2021-07-02 12:16:12 -0600

    connect to db

--- persist.js
+++ persist.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose");
 
 function connect(callback) {
-  callback();
+  let connectionString = `mongodb+srv://todo_2021:mycoolpassword@cluster0.jdtxt.mongodb.net/forum_2021?retryWrites=true&w=majority`;
+
+  console.log("connect to db...");
+
+  mongoose
+    .connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((err) => {
+      console.log("There was an error connecting to the db", err);
+    });
+
+  mongoose.connection.once("open", callback);
 }
 
 module.exports = connect;
--- server.js
+++ server.js
@@ -7,3 +7,15 @@ server.use(cors());
 server.use(express.json({}));
 
 module.exports = server;
+
+// GET /thread
+
+// GET /thread/:id
+
+// POST /thread
+
+// DELETE /thread/:id
+
+// POST /post
+
+// DELETE /post/:thread_id/:post_id
