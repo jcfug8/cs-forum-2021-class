3208ad17e19f32fa58102f7e5d7960090f042531
Jace Fugate <jace.fugate@tcnbroadcasting.com>
2021-07-06 10:35:06 -0600

    kind of implement methods

--- model.js
+++ model.js
@@ -1,1 +1,28 @@
-// put schemas here
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema(
+  {
+    author: String,
+    body: String,
+    thread_id: { type: mongoose.Schema.Types.ObjectId, ref: "Thread" },
+  },
+  { timestamps: true }
+);
+
+const threadSchema = mongoose.Schema(
+  {
+    author: String,
+    name: String,
+    description: String,
+    posts: [postSchema],
+    category: String,
+  },
+  { timestamps: true }
+);
+
+const Thread = mongoose.model("Thread", threadSchema);
+
+module.exports = {
+  Thread,
+  Post,
+};
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { Thread } = require("./model");
+const { json } = require("express");
 
 const server = express();
 
@@ -27,42 +29,120 @@ module.exports = server;
 server.get("/thread", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   console.log("getting all threads");
-  res.json([]);
+  Thread.find({}, (err, threads) => {
+    // check if error is null
+    if (err != null) {
+      res.status(500).json({
+        error: err,
+        message: "could not list threads",
+      });
+      return;
+    }
+    // success
+    res.status(200).json(threads);
+  });
 });
 
 // GET /thread/:id
 server.get("/thread/:id", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   console.log(`getting thread with id ${req.params.id}`);
-  res.json({});
+  Thread.findById(req.params.id, (err, thread) => {
+    if (err != null) {
+      // return error
+    } else if (thread === null) {
+      // return 404
+    }
+    // success
+  });
 });
 
 // POST /thread
 server.post("/thread", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   console.log(`creating thread with body`, req.body);
-  res.json({});
+  Thread.create(
+    {
+      // this is the thread we are creating
+    },
+    (err, thread) => {
+      if (err != null) {
+        // return error
+      }
+      // success
+    }
+  );
 });
 
 // DELETE /thread/:id
 server.delete("/thread/:id", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   console.log(`deleting thread with id ${req.params.id}`);
-  res.json({});
+  Thread.findByIdAndDelete(req.params.id, (err, thread) => {
+    if (err != null) {
+      // return error
+    } else if (thread === null) {
+      // return 404
+    }
+    // success
+  });
 });
 
 // POST /post
 server.post("/post", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   console.log(`creating post with body`, req.body);
-  res.json({});
+
+  let newPost = {
+    author: req.body.author || "",
+    body: req.body.body || "",
+    thread_id: req.body.thread_id || "",
+  };
+
+  Thread.findByIdAndUpdate(
+    req.body.thread_id,
+    {
+      $push: { posts: newPost },
+    },
+    {
+      new: true,
+    },
+    (err, thread) => {
+      if (err != null) {
+        // return error
+      } else if (thread === null) {
+        // return 404
+      }
+      // success
+    }
+  );
 });
 
 // DELETE /post/:thread_id/:post_id
-server.delete("/post/:thread_id/:post_id", (req, res) => {
+server.delete(" ", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   console.log(
     `deleting post with id ${req.params.post_id} on thread with id ${req.params.thread_id}`
   );
-  res.json({});
+  Thread.findByIdAndUpdate(
+    req.params.thread_id,
+    {
+      $pull: {
+        posts: {
+          _id: req.params.post_id,
+        },
+      },
+    },
+    {
+      new: true,
+    },
+    (err, thread) => {
+      if (err != null) {
+        // return error
+      } else if (thread === null) {
+        // return 404
+      }
+      // success
+    }
+  );
 });
