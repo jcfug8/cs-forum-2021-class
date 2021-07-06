6a438336ba62ae1f198b0450db287fb1c399d378
Jace Fugate <jace.fugate@tcnbroadcasting.com>
2021-07-06 09:49:19 -0600

    stub out methods

--- server.js
+++ server.js
@@ -24,28 +24,45 @@ server.use((req, res, next) => {
 module.exports = server;
 
 // GET /thread
-
-// this is where we will do our own middleware
-app.get("/thread", (req, res) => {
-  console.log(
-    "Time: ",
-    Date.now(),
-    " - Method: ",
-    req.method,
-    " - Path: ",
-    req.originalUrl,
-    " - Body: ",
-    req.body
-  );
-  next();
+server.get("/thread", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  console.log("getting all threads");
+  res.json([]);
 });
 
 // GET /thread/:id
+server.get("/thread/:id", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  console.log(`getting thread with id ${req.params.id}`);
+  res.json({});
+});
 
 // POST /thread
+server.post("/thread", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  console.log(`creating thread with body`, req.body);
+  res.json({});
+});
 
 // DELETE /thread/:id
+server.delete("/thread/:id", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  console.log(`deleting thread with id ${req.params.id}`);
+  res.json({});
+});
 
 // POST /post
+server.post("/post", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  console.log(`creating post with body`, req.body);
+  res.json({});
+});
 
 // DELETE /post/:thread_id/:post_id
+server.delete("/post/:thread_id/:post_id", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  console.log(
+    `deleting post with id ${req.params.post_id} on thread with id ${req.params.thread_id}`
+  );
+  res.json({});
+});
